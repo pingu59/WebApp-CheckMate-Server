51d79d48eedd343dee7f82d5142b6b52953725af
Tianyi Zuo <tz2617@sprite03.doc.ic.ac.uk>
2019-05-30 13:41:22 +0100

    Try if the pipeline afterscript is working

--- src/main/java/WebAppServer/App.java
+++ src/main/java/WebAppServer/App.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class App {
     public static void main(String[] args) {
-
         SpringApplication.run(App.class, args);
-
     }
 }
